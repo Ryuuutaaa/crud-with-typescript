bc2cf39cf53297b68d0b9f58e0f0116656a1dd64
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-06-04 01:38:04 +0800

    add compenent search and install dependence react-icons:

---
+++ components/search.tsx
@@ -0,0 +1,15 @@
+const Search = () => {
+  return (
+    <div className="relaative flex flex-1">
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="search"
+        className="w-full border-gray-200  py-2 pl-10 text-sm outline-2 rounded-sm"
+      />
+    </div>
+  );
+};
+
+export default Search;
