214c019eac39778f66594d00ca5a0701a487fcbd
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-07-10 17:14:03 +0800

    add seach and buttons components

--- components/search.tsx
+++ components/search.tsx
@@ -1,13 +1,14 @@
+import { IoSearch } from "react-icons/io5";
+
 const Search = () => {
   return (
-    <div className="relaative flex flex-1">
+    <div className="relative flex flex-1">
       <input
         type="text"
-        name=""
-        id=""
-        placeholder="search"
-        className="w-full border-gray-200  py-2 pl-10 text-sm outline-2 rounded-sm"
+        placeholder="search.."
+        className="w-full border border-gray-700 py-2 pl-10 text-sm outline-2 rounded-sm"
       />
+      <IoSearch className="absolute left-3 top-2 h-5 w-5 text-gray-500" />
     </div>
   );
 };
