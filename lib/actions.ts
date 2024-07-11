c345ca02bdd27f5701703f9207cb5a04ff6facf5
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-07-12 02:23:14 +0800

    zoid

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,6 +1,19 @@
 "use server";
 
+import { z } from "zod";
+
+const ContactSchema = z.object({
+  name: z.string().min(6),
+  phone: z.string().min(11),
+});
+
 export const saveContact = async (formData: FormData) => {
-  const data = Object.fromEntries(formData.entries());
-  console.log(data);
+  const ValidateFields = ContactSchema.safeParse(
+    Object.fromEntries(formData.entries())
+  );
+  if (!ValidateFields.success) {
+    return {
+      Error: ValidateFields.error.flatten().fieldErrors,
+    };
+  }
 };
