14771a817d0c2d7c2343d29b8961801b25aa4912
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-07-11 10:47:53 +0800

    add input value

--- components/create-form.tsx
+++ components/create-form.tsx
@@ -1,30 +1,36 @@
-import React from "react";
+import { saveContact } from "@/lib/actions";
 
 const CreateForm = () => {
   return (
     <div>
-      <form action="">
+      <form action={saveContact}>
         <div className="mb-5">
-          <label htmlFor="name">
-            <input
-              type="text"
-              name="name"
-              id="name"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-              placeholder="Fullname..."
-            />
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium text-gray-900">
+            Fullname
           </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="Fullname..."
+          />
         </div>
         <div className="mb-5">
-          <label htmlFor="phone">
-            <input
-              type="text"
-              name="phone"
-              id="phone"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-              placeholder="Phone Number..."
-            />
+          <label
+            htmlFor="phone"
+            className="block text-sm font-medium text-gray-900">
+            Phone Number
           </label>
+          <input
+            type="text"
+            name="phone"
+            id="phone"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="Phone Number..."
+          />
         </div>
         <button
           type="submit"
