26da4da7e8a82891472714f074b67a48d021057c
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-07-12 11:17:48 +0800

    add erorr message and testing create

--- components/create-form.tsx
+++ components/create-form.tsx
@@ -22,6 +22,9 @@ const CreateForm = () => {
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             placeholder="Fullname..."
           />
+          <div id="name-error" aria-live="polite" aria-atomic="true">
+            <p className="mt-2 text-sm text-red-500">{state?.Error?.name}</p>
+          </div>
         </div>
         <div className="mb-5">
           <label
@@ -36,6 +39,12 @@ const CreateForm = () => {
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             placeholder="Phone Number..."
           />
+          <div id="phone-error" aria-live="polite" aria-atomic="true">
+            <p className="mt-2 text-sm text-red-500">{state?.Error?.phone}</p>
+          </div>
+          <div id="message-error" aria-live="polite" aria-atomic="true">
+            <p className="mt-2 text-sm text-red-500">{state?.message}</p>
+          </div>
         </div>
         <button
           type="submit"
