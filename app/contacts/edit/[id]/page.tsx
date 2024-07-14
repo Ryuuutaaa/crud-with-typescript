13ddce6bdf4b73f25e4e8b586a5809c799ca30b4
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-07-15 01:16:13 +0800

    read contact by id

--- app/contacts/edit/[id]/page.tsx
+++ app/contacts/edit/[id]/page.tsx
@@ -1,10 +1,19 @@
 import UpdateForm from "@/components/edit-form";
+import { getContactById } from "@/lib/data";
+import { notFound } from "next/navigation";
 
-const UpdateContactPage = () => {
+const UpdateContactPage = async ({ params }: { params: { id: string } }) => {
+  const id = params.id;
+  const contact = await getContactById(id);
+
+  if (!contact) {
+    notFound();
+  }
+
   return (
     <div className="max-w-md mx-auto mt-5">
       <h1 className="text-2xl text-center mb-2">Update Contact</h1>
-      <UpdateForm />
+      <UpdateForm contact={contact} />
     </div>
   );
 };
--- components/edit-form.tsx
+++ components/edit-form.tsx
@@ -3,8 +3,9 @@
 import { saveContact } from "@/lib/actions";
 import { useFormState } from "react-dom";
 import { SubmitButton } from "./buttons";
+import type { Contact } from "@prisma/client";
 
-const UpdateForm = () => {
+const UpdateForm = ({ contact }: { contact: Contact }) => {
   const [state, formAction] = useFormState(saveContact, null);
 
   return (
@@ -22,6 +23,7 @@ const UpdateForm = () => {
             id="name"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             placeholder="Fullname..."
+            defaultValue={contact.name}
           />
           <div id="name-error" aria-live="polite" aria-atomic="true">
             <p className="mt-2 text-sm text-red-500">{state?.Error?.name}</p>
@@ -39,6 +41,7 @@ const UpdateForm = () => {
             id="phone"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             placeholder="Phone Number..."
+            defaultValue={contact.phone}
           />
           <div id="phone-error" aria-live="polite" aria-atomic="true">
             <p className="mt-2 text-sm text-red-500">{state?.Error?.phone}</p>
