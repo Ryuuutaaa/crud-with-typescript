e8a6c45000529b504e1536927b54b3a1223c2c50
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-07-17 00:22:06 +0800

    addjustment search bar

--- components/search.tsx
+++ components/search.tsx
@@ -10,6 +10,13 @@ const Search = () => {
 
   const handleSearch = (term: string) => {
     console.log(term);
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
   };
   return (
     <div className="relative flex flex-1">
@@ -18,6 +25,7 @@ const Search = () => {
         placeholder="search.."
         className="w-full border border-gray-700 py-2 pl-10 text-sm outline-2 rounded-sm"
         onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get("query")?.toString()}
       />
       <IoSearch className="absolute left-3 top-2 h-5 w-5 text-gray-500" />
     </div>
