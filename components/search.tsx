1ec61bd27463a2b145b3a8b2d516636a491573bb
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-07-17 00:08:24 +0800

    console log search in serach bar

--- components/search.tsx
+++ components/search.tsx
@@ -1,12 +1,18 @@
+"use client";
+
 import { IoSearch } from "react-icons/io5";
 
 const Search = () => {
+  const handleSearch = (term: string) => {
+    console.log(term);
+  };
   return (
     <div className="relative flex flex-1">
       <input
         type="text"
         placeholder="search.."
         className="w-full border border-gray-700 py-2 pl-10 text-sm outline-2 rounded-sm"
+        onChange={(e) => handleSearch(e.target.value)}
       />
       <IoSearch className="absolute left-3 top-2 h-5 w-5 text-gray-500" />
     </div>
